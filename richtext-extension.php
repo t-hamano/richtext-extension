58e9df1b0278b93ce4a19a6ee2762b28f3df1104
Tetsuaki Hamano <tetsuaki.hamano@gmail.com>
2020-05-03 18:34:54 +0900

    update version

--- richtext-extension.php
+++ richtext-extension.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: RichText Extension
  * Description: Adds useful decoration features to the Gutenberg RichText editor toolbar.
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Tetsuaki Hamano
  * Author URI: https://github.com/t-hamano
  * License: GPL2 or later
@@ -19,7 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'RTEX_VERSION', '0.0.1' );
+define( 'RTEX_VERSION', '0.0.2' );
 define( 'RTEX_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
 define( 'RTEX_URL', untrailingslashit( plugin_dir_url( __FILE__ ) ) );
 define( 'RTEX_BASENAME', plugin_basename( __FILE__ ) );
