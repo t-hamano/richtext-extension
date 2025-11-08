bc25c2c01634cce13859e7f22d42f615f11e0efe
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-11-08 20:52:09 +0900

    v2.9.0

--- richtext-extension.php
+++ richtext-extension.php
@@ -4,7 +4,7 @@
  * Description: Adds useful decoration features to the Gutenberg RichText editor toolbar.
  * Requires at least: 6.8
  * Requires PHP: 8.0
- * Version: 2.8.0
+ * Version: 2.9.0
  * Author: Aki Hamano
  * Author URI: https://github.com/t-hamano
  * License: GPL2 or later
